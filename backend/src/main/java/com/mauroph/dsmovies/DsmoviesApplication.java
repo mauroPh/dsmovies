671d249b6a22948c59c8e4c6f63ccc4b9253f583
mauroPh <mauro.philipe.santos@gmail.com>
2022-03-11 00:05:55 -0300

    Project created

---
+++ backend/src/main/java/com/mauroph/dsmovies/DsmoviesApplication.java
@@ -0,0 +1,13 @@
+package com.mauroph.dsmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoviesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mauroph/dsmovies/DsmoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauroph.dsmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
