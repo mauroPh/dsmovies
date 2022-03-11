38621e896b863663fd9f40249d86a35b571876eb
mauroPh <mauro.philipe.santos@gmail.com>
2022-03-11 00:59:06 -0300

    Form Layout

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,49 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+
+function Form(){
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/original/kxFxtYdaIaGsp3N9KEtPSj7yqEE.jpg",
+        title: "Spiderman - No Way Home",
+        count: 2,
+        score: 4.5
+    };
+
+    return(
+
+        <div className="dsmovie-form-container">
+        <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
+        <div className="dsmovie-card-bottom-container">
+            <h3>{movie.title}</h3>
+            <form className="dsmovie-form">
+                <div className="form-group dsmovie-form-group">
+                    <label htmlFor="email">Informe seu email</label>
+                    <input type="email" className="form-control" id="email" />
+                </div>
+                <div className="form-group dsmovie-form-group">
+                    <label htmlFor="score">Informe sua avaliação</label>
+                    <select className="form-control" id="score">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+                <div className="dsmovie-form-btn-container">
+                    <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                </div>
+            </form >
+            <Link to="/">
+            <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+
+            </Link>
+        </div >
+    </div >
+    );
+}
+
+export default Form;
